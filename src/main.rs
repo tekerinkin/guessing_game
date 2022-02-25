7db551f175931a95dc8222b8d3dbd1283feabb6b
tekerinkin <dmshestakov@icloud.com>
2022-02-25 17:19:17 +0300

    complete game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("Input your guess, please. ");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Problem with reading");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small number!"),
+            Ordering::Greater => println!("Too big number!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
